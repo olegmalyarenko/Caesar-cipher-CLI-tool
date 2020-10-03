54b495163bd269e39137fe496509a823b3acfcd3
olegmalyarenko <oi.malyarenko@gmail.com>
2020-10-03 20:44:27 +0300

    feat: add encode/decode func

--- caesar-cipher-cli/cipher.js
+++ caesar-cipher-cli/cipher.js
@@ -1,8 +1,15 @@
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 const fullAlphabet = alphabet + alphabet + alphabet;
 
-function runCipher( chunk, shift ){
+function runCipher( chunk, shift, actionVal ){
+  console.log('action cipher', typeof actionVal); //encode/decode 
+  if (actionVal != 'encode' && actionVal != 'decode') {
+    console.error('Write correct action!');
+    return; 
+  }
+  console.log('shift cipher', shift);
   let textChunk = chunk.toString();
+  console.log('chank cipher', textChunk);
   const cipherText = textChunk.toLowerCase();
   const cipherOffset = Number(shift.toString());
   //cipherOffset = (cipherOffset % alphabet.length);
@@ -15,10 +22,11 @@ function runCipher( chunk, shift ){
     
      if(index == -1){
        cipherFinish += letter;
-     } else {
-       index = Number(index) + Number(cipherOffset) + Number(alphabet.length);
+     } else { 
+       if(actionVal === 'encode') index = Number(index) + Number(cipherOffset) + Number(alphabet.length);
+       if(actionVal === 'decode') index = Number(index) - Number(cipherOffset) + Number(alphabet.length);
        let nextLetter = fullAlphabet[index];
-       console.log('stringch', textChunk[i]);
+       //console.log('stringch', textChunk[i]);
        if(textChunk[i] === letter.toUpperCase()) nextLetter = nextLetter.toUpperCase();
        cipherFinish += nextLetter;
      }
--- caesar-cipher-cli/index.js
+++ caesar-cipher-cli/index.js
@@ -1,18 +1,48 @@
 const  cipher = require('./cipher.js');
 const { pipeline } = require('stream');
+const { Command, action } = require('commander');
+const program = new Command();
 const arguments = process.argv; 
 const fs = require('fs');
 
 const file = '../caesar-cipher-cli/input.txt';
 const Transform = require('stream').Transform;
-const shift = arguments[2];
-console.log('chunk.toString', shift);
 
 
-const getTransformStream = () => {
+program
+  .storeOptionsAsProperties(false)
+  .passCommandToAction(false);
+
+
+
+program
+   .option('-a, --action <value>', 'action')
+   .option('-s, --shift <number>', 'shift')
+   .option('-i, --input <path>', 'input file')
+   .option('-o, --output <path>', 'output file');
+
+  
+ 
+program.parse(process.argv);
+console.log(program.opts()); 
+let shift = program.opts().shift;
+let actionVal = program.opts().action;
+
+
+
+  console.log('shift', shift);
+  console.log('ACTION', actionVal); 
+
+if (program.input) console.log(`input- ${program.input}`);
+if (program.output) console.log(`input- ${program.output}`);
+
+
+const getTransformStream = (shift, actionVal) => {
     return new Transform({
       transform(chunk) {
-        this.push(cipher.runCipher(chunk, shift));
+      
+        this.push(cipher.runCipher(chunk, shift, actionVal));
+        
         //callback();
       }
     });
@@ -21,7 +51,7 @@ const getTransformStream = () => {
 
 pipeline(
   fs.createReadStream(file, 'utf8'),
-  getTransformStream(),
+  getTransformStream(shift, actionVal),
   fs.createWriteStream(__dirname + '/output.txt'),
   (err) => {
     if (err) {
